cf0e837e9896fbd60587f36977ee2b3c23a13ee4
mr9 asif <asifalibd002@gmail.com>
2026-05-23 17:19:01 +0600

    finish all

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
 import authRouter from './modules/auth/auth.routes.js';
 import issueRouter from './modules/issues/issues.router.js';
 
@@ -7,6 +7,9 @@ const app =express();
 
 app.use(express.json());
 
+app.get('/', (req:Request,res:Response)=>{
+    res.send("server working properly!")
+})
 app.use('/api/auth', authRouter)
 app.use('/api', issueRouter)
 
